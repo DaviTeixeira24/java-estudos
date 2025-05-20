6bb773ed92bdd1b98d34e6c5dda527b941bed6e6
DaviTeixeira24 <davisilvateixeiramoreira@gmail.com>
2025-05-20 14:43:24 -0300

    version 1.0

---
+++ exercicios/exercicios-java-lacos-repeticao/for/ExerciciosFor/src/Exercicio1.java
@@ -0,0 +1,21 @@
+/* Tabuada com For:
+Escreva um programa que receba um número inteiro do 
+usuário e exiba sua tabuada de 1 a 10 usando um loop for.*/
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite um numero inteiro: ");
+    int numero = scanner.nextInt();
+
+    System.out.println("Tabada do numero digitado: ");
+
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(numero + " X " + i + " = " + (numero * i));
+    }
+
+  }
+}
---
+++ exercicios/exercicios-java-lacos-repeticao/for/ExerciciosFor/src/Exercicio2.java
@@ -0,0 +1,18 @@
+/* Soma de Números Pares
+Crie um programa que use for para somar todos os números 
+ pares de 1 a 100 e exiba o resultado final.*/
+
+public class Exercicio2 {
+  public static void main(String[] args) {
+
+    int soma = 0;
+
+    for (int i = 1; i <= 100; i++) {
+      if (i % 2 == 0) {
+        soma += i; // abreviação de "soma = i + i"
+      }
+
+    }
+    System.out.println("Soma dos numeros pares: " + soma);
+  }
+}
